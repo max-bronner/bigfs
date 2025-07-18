899e4c61fe616f37ea311da091c927e9b68725e0
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-07-18 19:04:42 +0200

    Add parsing of big files to virtual file system

--- src/big/virtualFileSystem.ts
+++ src/big/virtualFileSystem.ts
@@ -1,8 +1,28 @@
 import { workspace, EventEmitter, FileType, Uri } from 'vscode';
 import { BIG_PATTERN } from '../constants';
+import { parseBigArchive } from './bigParser';
+import type { BigFileArchive } from './bigParser';
+
+export class FileService {
+  private _onDidChangeArchives = new EventEmitter<Uri>();
+  public readonly onDidChangeArchives = this._onDidChangeArchives.event;
 
   constructor() {
     this.scanWorkspace();
+  }
+
+  private async parseArchiveFile(uri: Uri): Promise<BigFileArchive> {
+    try {
+      const byteArray = await workspace.fs.readFile(uri);
+      const buffer = Buffer.from(byteArray);
+      return parseBigArchive(buffer);
+    } catch (error) {
+      throw Error(`Failed to parse archive ${uri.fsPath}:`);
+    }
+  }
+
+  private async addArchiveToVirtualFileTree(uri: Uri): Promise<void> {
+    const data = await this.parseArchiveFile(uri);
   }
 
   public async scanWorkspace(): Promise<void> {
@@ -13,7 +33,7 @@ import { BIG_PATTERN } from '../constants';
     const archiveUri = await workspace.findFiles(BIG_PATTERN, null, 100);
 
     archiveUri.forEach((uri) => {
-      // Todo: add parsing of files
+      this.addArchiveToVirtualFileTree(uri);
     });
   }
 }
