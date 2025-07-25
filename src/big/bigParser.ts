1d5c76f8140ae46e46c3f755e8918fed8e88f317
Max Bronner <88713269+max-bronner@users.noreply.github.com>
2025-07-25 20:45:25 +0200

    Add writer for big archives

--- src/big/bigParser.ts
+++ src/big/bigParser.ts
@@ -15,6 +15,10 @@ export interface BigFileArchive {
 
 const LENGTH_HEADER = 16;
 
+const alignBytes = (offset: number): number => {
+  return (offset + 3) & ~3;
+};
+
 const readHeaders = (buffer: NonSharedBuffer) => {
   // Read header (16 bytes total)
   const magic = buffer.toString('ascii', 0, 4);
@@ -91,4 +95,67 @@ export const parseBigArchive = (buffer: NonSharedBuffer): BigFileArchive => {
     indexOffset,
     entries,
   };
+};
+
+const calculateBufferSizes = (entries: BigFileEntry[]): number[] => {
+  let totalMetaSize = 0;
+  let totalDataSize = 0;
+  entries.forEach((entry, index) => {
+    const isLast = index === entries.length - 1;
+
+    const nameLength = Buffer.byteLength(entry.name, 'utf-8');
+    const entryMetaSize = 8 + nameLength + (isLast ? 0 : 1);
+    totalMetaSize += entryMetaSize;
+
+    const entryDataSize = isLast
+      ? entry.fileBuffer.length
+      : alignBytes(entry.fileBuffer.length);
+    totalDataSize += entryDataSize;
+  });
+
+  return [LENGTH_HEADER, alignBytes(totalMetaSize), totalDataSize];
+};
+
+const writeIndexEntry = (
+  buffer: Buffer,
+  offset: number,
+  entry: BigFileEntry
+): number => {
+  buffer.writeUInt32BE(entry.offset, offset);
+  buffer.writeUInt32BE(entry.size, offset + 4);
+
+  const nameBytes = Buffer.from(entry.name, 'utf-8');
+  nameBytes.copy(buffer, offset + 8);
+  buffer[offset + 8 + nameBytes.length] = 0; // null terminator
+
+  return offset + 8 + nameBytes.length + 1;
+};
+
+export const writeBigArchive = (archive: BigFileArchive): Buffer => {
+  const [headerSize, metaSize, dataSize] = calculateBufferSizes(
+    archive.entries
+  );
+
+  const dataOffset = headerSize + metaSize;
+
+  const totalSize = headerSize + metaSize + dataSize;
+  const buffer = Buffer.alloc(totalSize);
+
+  buffer.write(archive.magic, 0, 4, 'ascii');
+  buffer.writeUInt32LE(totalSize, 4);
+  buffer.writeUInt32BE(archive.entries.length, 8);
+  buffer.writeUInt32BE(dataOffset - 1, 12); // indexOffset
+
+  let currentMetaOffset = LENGTH_HEADER;
+  let currentDataOffset = dataOffset;
+  archive.entries.forEach((entry) => {
+    entry.offset = currentDataOffset;
+    entry.size = entry.fileBuffer.length;
+    currentMetaOffset = writeIndexEntry(buffer, currentMetaOffset, entry);
+
+    buffer.set(entry.fileBuffer, dataOffset);
+    currentDataOffset += alignBytes(entry.fileBuffer.length);
+  });
+
+  return buffer;
 };
